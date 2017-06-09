59ae92bcd847ae5052ec50bf7870e0f656c81404
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-06-09 18:51:30 +0300

    Add new syntax for events

--- .rispa/activator.js
+++ .rispa/activator.js
@@ -1,3 +1,5 @@
+import { init, prepare, build } from '@rispa/core/events'
+import { server } from '@rispa/server/events'
 import { createConfig } from '@webpack-blocks/webpack2'
 import universalSettings from '../universal-webpack-settings'
 import webpackExtentionClient from './client.wpc'
@@ -5,25 +7,24 @@ import clientConfiguration from '../src/configuration/client'
 import renderMiddleware from '../src/middleware'
 
 const activator = on => {
-  on('init', (command, registry) => {
-    if (command === 'server' || command === 'build') {
-      registry.add('webpack.client', webpackExtentionClient)
-    }
-
-    if (command === 'server') {
-      registry.set('render', renderMiddleware)
-    }
+  on(init(server), (command, registry) => {
+    registry.add('webpack.client', webpackExtentionClient)
+    registry.set('render', renderMiddleware)
   })
 
-  on('prepare', (command, registry) => {
-    if (command === 'server' || command === 'build') {
-      const webpackConfig = createConfig(registry.get('webpack.client') || [])
-      registry.set('webpack.client', [() => clientConfiguration(
-        webpackConfig,
-        universalSettings,
-      )])
-    }
+  on(init(build), (command, registry) => {
+    registry.add('webpack.client', webpackExtentionClient)
   })
+
+  const prepareHandler = (command, registry) => {
+    const webpackConfig = createConfig(registry.get('webpack.client') || [])
+    registry.set('webpack.client', [() => clientConfiguration(
+      webpackConfig,
+      universalSettings,
+    )])
+  }
+  on(prepare(server), prepareHandler)
+  on(prepare(build), prepareHandler)
 }
 
 export default activator
