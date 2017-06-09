4dad028dc8c5bd03fcb1ec25c2540fa5f17f917f
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-06-09 18:31:09 +0300

    Use new syntax for core events

--- .rispa/activator.js
+++ .rispa/activator.js
@@ -1,19 +1,20 @@
-import build from '../src/build'
+import { init, start, build } from '@rispa/core/events'
+import { server } from '@rispa/server/events'
+import runBuild from '../src/build'
 import webpackExtensionCommon from './common.wpc'
 import webpackExtensionClient from './client.wpc'
 
 const activator = on => {
-  on('init', (command, registry) => {
-    if (command === 'build' || command === 'server') {
-      registry.add('webpack.common', webpackExtensionCommon)
-      registry.add('webpack.client', webpackExtensionCommon, webpackExtensionClient)
-    }
-  })
+  const initHandler = registry => {
+    registry.add('webpack.common', webpackExtensionCommon)
+    registry.add('webpack.client', webpackExtensionCommon, webpackExtensionClient)
+  }
 
-  on('start', (command, registry) => {
-    if (command === 'build') {
-      build(registry.get('webpack.client'))
-    }
+  on(init(build), initHandler)
+  on(init(server), initHandler)
+
+  on(start(build), (command, registry) => {
+    runBuild(registry.get('webpack.client'))
   })
 }
 
